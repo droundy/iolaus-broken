9df0c976e58f3c16e3eeb32e2bd21d8f07fb3b6c
David Roundy <droundy@abridgegame.org>
2009-09-21 10:53:44 -0400

    move commit-tree into plumbing.

--- Git/Plumbing.hs
+++ Git/Plumbing.hs
@@ -1,8 +1,7 @@
 module Git.Plumbing ( lsfiles, updateindex, writetree, updateref,
-                      headhash,
-                      cleanhash ) where
+                      headhash, commitTree ) where
 
-import System.IO ( hGetContents )
+import System.IO ( hGetContents, hPutStr, hClose )
 -- import System.IO.Pipe ( openPipe )
 import System.Exit ( ExitCode(..) )
 import System.Process.Redirects ( createProcess, waitForProcess, proc,
@@ -55,6 +54,26 @@ updateref r h =
        case ec of
          ExitSuccess -> return ()
          ExitFailure _ -> fail "git-update-ref failed"
+
+commitTree :: String -> [String] -> String -> IO String
+commitTree t pars m =
+    do let pflags = concatMap (\p -> ["-p",p]) pars
+       (Just i, Just o, Nothing, pid) <-
+           createProcess (proc "git-commit-tree" (t:pflags)) {
+               env = Just [("GIT_AUTHOR_NAME","David Roundy"),
+                           ("GIT_AUTHOR_EMAIL", "droundy@abridgegame.org"),
+                           ("GIT_COMMITTER_NAME","David Roundy"),
+                           ("GIT_COMMITTER_EMAIL", "droundy@abridgegame.org")],
+               std_out = CreatePipe,
+               std_in = CreatePipe }
+       out <- hGetContents o
+       hPutStr i m
+       hClose i
+       ec <- length out `seq` waitForProcess pid
+       case ec of
+         ExitSuccess -> return $ cleanhash out
+         ExitFailure _ -> fail "git-commit-tree failed"
+
 
 cleanhash :: String -> String
 cleanhash = take 40
--- arcs.hs
+++ arcs.hs
@@ -2,14 +2,9 @@ module Main where
 
 import Control.Monad ( when )
 import System.Environment ( getArgs )
-import System.IO ( hGetContents, hPutStrLn, hClose )
--- import System.IO.Pipe ( openPipe )
-import System.Exit ( ExitCode(..) )
-import System.Process.Redirects ( createProcess, waitForProcess, proc,
-                                  CreateProcess(..), StdStream(..) )
 
 import Git.Plumbing ( lsfiles, writetree, updateindex, updateref,
-                      headhash, cleanhash )
+                      commitTree, headhash )
 
 email :: String
 email = "roundyd@physics.oregonstate.edu"
@@ -21,26 +16,9 @@ commit message =
        t <- writetree
        putStrLn ("write-tree gives "++show t)
        par <- headhash
-       (Just i, Just o, Nothing, pid) <-
-           createProcess (proc "git-commit-tree" [t,"-p",par]) {
-                  env = Just [("GIT_AUTHOR_NAME","David Roundy"),
-                              ("GIT_AUTHOR_EMAIL", email),
-                              ("GIT_AUTHOR_DATE","Today"),
-                              ("GIT_COMMITTER_NAME","David Roundy"),
-                              ("GIT_COMMITTER_EMAIL", email),
-                              ("GIT_COMMITTER_DATE","Today")],
-                  std_in = CreatePipe,
-                  std_out = CreatePipe
-                }
-       hPutStrLn i message
-       out <- hGetContents o
-       hClose i
-       ec <- length out `seq` waitForProcess pid
-       case ec of
-         ExitSuccess -> return ()
-         ExitFailure _ -> fail "git-commit-tree failed"
-       putStrLn ("commit-tree gives "++ cleanhash out)
-       updateref "refs/heads/master" $ cleanhash out
+       com <- commitTree t [par] message
+       putStrLn ("commit-tree gives "++ com)
+       updateref "refs/heads/master" com
 
 main :: IO ()
 main = do x <- getArgs
