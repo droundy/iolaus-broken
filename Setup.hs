d6bbdcef7fb0fdcdac707f0fe92594c1bf09242f
David Roundy <roundyd@physics.oregonstate.edu>
2009-09-26 10:22:01 -0400

    have install install bash-completion script.

--- Setup.hs
+++ Setup.hs
@@ -4,5 +4,6 @@ import Distribution.Franchise.V1
 main = build [] $
        do hcFlags ["-Wall","-Iinclude"]
           ghcFlags ["-threaded"]
+          withDirectory "etc" $ etc "bash_completion.d/arcs"
           withModule "System.Process.Redirects" $ define "HAVE_REDIRECTS"
           executable "arcs" "arcs.hs" []
