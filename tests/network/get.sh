90915281dfb9598f5ac14ccf641155c84151cd8f
David Roundy <roundyd@physics.oregonstate.edu>
2009-09-30 09:14:34 -0400

    fix broken network-get test.

--- tests/network/get.sh
+++ tests/network/get.sh
@@ -1,8 +1,6 @@
 #!/usr/bin/env bash
 set -ev
 
-echo this test-fails because get is not yet implemented
-
-iolaus get http://iolaus.abridgegame.org iolaus
-test -d iolaus/.git
-test -f iolaus/iolaus.hs
+iolaus get git://gitorious.org/ditz/mainline.git test
+test -d test/.git
+#test -f iolaus/iolaus.hs
